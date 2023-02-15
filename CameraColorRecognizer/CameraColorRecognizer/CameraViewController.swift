469a5c7063d5f7fcdf097cb52b2dfa717f0fc3a9
David Noy <>
2023-02-15 13:50:30 +0200

    First commit + views

---
+++ CameraColorRecognizer/CameraColorRecognizer/CameraViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CameraColorRecognizer
+//
+//  Created by דוד נוי on 15/02/2023.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet private weak var cameraViewContainer: UIView!
+    @IBOutlet private weak var firstColorLabel: UILabel!
+    @IBOutlet private weak var secondColorLabel: UILabel!
+    @IBOutlet private weak var thirdColorLabel: UILabel!
+    @IBOutlet private weak var fourthColorLabel: UILabel!
+    @IBOutlet private weak var fifthColorLabel: UILabel!
+     
+    @IBOutlet private weak var firstRGB: UILabel!
+    @IBOutlet private weak var secondRGB: UILabel!
+    @IBOutlet private weak var thirdRGB: UILabel!
+    @IBOutlet private weak var fourthRGB: UILabel!
+    @IBOutlet private weak var fifthRGB: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
+
